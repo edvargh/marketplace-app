77a988facc0e2453fef4df36892cef683856a6a3
Edvard Granheim Harbo <edvargh@stud.ntnu.no>
2025-03-28 17:05:08 +0100

    Added Crossorigin in AuthController

--- backend/src/main/java/com/marketplace/backend/controller/AuthController.java
+++ backend/src/main/java/com/marketplace/backend/controller/AuthController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 
 public class AuthController {
 
