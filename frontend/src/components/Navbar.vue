11280aa7f23f7454430ccbde1905c6c20677c2e3
Tam Le <tamle2107@hotmail.com>
2025-03-29 13:12:15 +0100

    Feat/(Implemented the logout button and fixed the look on the navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,25 +1,42 @@
 <template>
   <nav class="navbar">
-    <router-link to="/" >Home</router-link>
+    <div class="nav-left">
+      <CustomButton>
+        <router-link to="/" class ="navbar-home-link">Home</router-link>
+      </CustomButton>
+    </div>
 
-      <ul>
-        <template v-if="isAuthenticated">
-          <li><button @click="handleLogout" class="logout-button">Log Out</button></li>
-          <li><router-link to="/profile">Profile</router-link></li>
-        </template>
-
-        <template v-else>
-          <li><router-link to="/login">login</router-link></li>
-        </template>
-      </ul>
-    </nav>
+    <ul class="nav-right">
+      <template v-if="isAuthenticated">
+        <li class="account-dropdown" ref="dropdownRef">
+          <div class = "dropdown-wrapper">
+          <CustomButton @click= "toggleDropdown"> Account </CustomButton>
+            <ul v-if="dropdownOpen" class="dropdown-menu">
+              <li><router-link to="/profile" class="dropdown-profile-link">Profile</router-link></li>
+              <li><button @click.stop="handleLogout" class="dropdown-logout-button">Log Out</button></li>
+            </ul>
+          </div>
+        </li>
+      </template>
+      <template v-else>
+        <li>
+          <CustomButton>
+          <router-link to="/login" class="navbar-login-button">
+            Log in
+          </router-link>
+        </CustomButton>
+        </li>
+      </template>
+    </ul>
+  </nav>
 </template>
 
 
 <script setup>
 import { useUserStore } from '@/stores/userStore'
-import { computed } from 'vue'
+import { computed, ref, onMounted, onUnmounted } from 'vue'
 import { useRouter } from 'vue-router'
+import CustomButton from '@/components/CustomButton.vue'
 
 const userStore = useUserStore()
 const router = useRouter()
@@ -27,9 +44,30 @@ const router = useRouter()
 const isAuthenticated = computed(() => userStore.isAuthenticated)
 
 const handleLogout = async () => {
-  await userStore.logout()
-  await router.push('/login')
+  userStore.logout()
+  router.push('/login')
 }
+
+const dropdownOpen = ref(false)
+const dropdownRef = ref(null)
+
+const toggleDropdown = () => {
+  dropdownOpen.value = !dropdownOpen.value
+}
+
+const handleClickOutside = (event) => {
+  if (dropdownRef.value && !dropdownRef.value.contains(event.target)) {
+    dropdownOpen.value = false
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('click', handleClickOutside)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('click', handleClickOutside)
+})
 </script>
 
 <style scoped>
